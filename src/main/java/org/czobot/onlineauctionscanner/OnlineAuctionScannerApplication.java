0b7e1f99d25437cc2bf46cd55b92bddfe86347bd
Stanisław Czobot <stac@netcompany.com>
2023-01-21 20:02:45 +0100

    Project initial commit

---
+++ src/main/java/org/czobot/onlineauctionscanner/OnlineAuctionScannerApplication.java
@@ -0,0 +1,13 @@
+package org.czobot.onlineauctionscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineAuctionScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineAuctionScannerApplication.class, args);
+	}
+
+}
